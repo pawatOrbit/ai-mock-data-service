e9b453d783a18d3cff0703f876c2bb123082fe3d
Pawat Thirasakthana <pawat.t@OB-PC-24-025.local>
2025-05-15 22:50:31 +0700

    refactor(mock-data):refactor config file.[MDA-001]

--- app/core/pgdb/db.py
+++ app/core/pgdb/db.py
@@ -1,6 +1,6 @@
 import databases
 from app.utils.path_builder import build_sql_postgres_connection_string
-from app.config import userNamedb, password, host, port, database_name
+from app.core.config.config import userNamedb, password, host, port, database_name
 
 connectionString = build_sql_postgres_connection_string(
     host=host,
